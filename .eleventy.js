864721bf2de6ac6afa527cca1b7880ddc4aedc90
Juan Fernandes <juan@fernandes.fyi>
2019-04-18 18:32:19 +0100

    Apply code cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,16 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // Pass through
-  eleventyConfig.addPassthroughCopy("src/assets/imgs");
-  eleventyConfig.addPassthroughCopy("src/assets/js/libs");
-  eleventyConfig.addPassthroughCopy("src/README.md");
-  eleventyConfig.addPassthroughCopy("src/sw.js");
-  eleventyConfig.addPassthroughCopy("src/sitemap.xml");
-  eleventyConfig.addPassthroughCopy("src/robots.txt");
-  eleventyConfig.addPassthroughCopy("src/keybase.txt");
-  eleventyConfig.addPassthroughCopy("src/humans.txt");
-  eleventyConfig.addPassthroughCopy("src/site.webmanifest");
-  eleventyConfig.addPassthroughCopy("src/browserconfig.xml");
-  eleventyConfig.addPassthroughCopy("src/.htaccess");
+  eleventyConfig.addPassthroughCopy('src/assets/imgs')
+  eleventyConfig.addPassthroughCopy('src/assets/js/libs')
+  eleventyConfig.addPassthroughCopy('src/README.md')
+  eleventyConfig.addPassthroughCopy('src/sw.js')
+  eleventyConfig.addPassthroughCopy('src/sitemap.xml')
+  eleventyConfig.addPassthroughCopy('src/robots.txt')
+  eleventyConfig.addPassthroughCopy('src/keybase.txt')
+  eleventyConfig.addPassthroughCopy('src/humans.txt')
+  eleventyConfig.addPassthroughCopy('src/site.webmanifest')
+  eleventyConfig.addPassthroughCopy('src/browserconfig.xml')
+  eleventyConfig.addPassthroughCopy('src/.htaccess')
 
   // module.exports = function(eleventyConfig) {
   //   eleventyConfig.setBrowserSyncConfig({
@@ -20,12 +20,12 @@ module.exports = function(eleventyConfig) {
 
   return {
     dir: {
-      input: "src",
-      output: "dist",
+      input: 'src',
+      output: 'dist'
     },
     passthroughFileCopy: true,
-    htmlTemplateEngine : "njk",
-    markdownTemplateEngine : "njk",
-    templateFormats : ["njk", "md", "html"],
-  };
-};
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
+    templateFormats: ['njk', 'md', 'html']
+  }
+}
