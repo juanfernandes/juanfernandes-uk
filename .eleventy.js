970f0c76fa6bc93e93625491a81fe2b15c7a8be4
Juan Fernandes <juan@fernandes.fyi>
2019-04-30 07:17:45 +0100

    fixup! Remove unused JS libs

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,6 @@ module.exports = function (eleventyConfig) {
   // Pass through
   eleventyConfig.addPassthroughCopy('README.md')
   eleventyConfig.addPassthroughCopy('src/assets/imgs')
-  eleventyConfig.addPassthroughCopy('src/assets/js/libs')
   eleventyConfig.addPassthroughCopy('src/sw.js')
   eleventyConfig.addPassthroughCopy('src/sitemap.xml')
   eleventyConfig.addPassthroughCopy('src/robots.txt')
