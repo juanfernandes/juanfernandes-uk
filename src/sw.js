0ee3940a3b6cb2db1db6140b44d60f3d950608c3
Juan Fernandes <juan@fernandes.fyi>
2019-04-30 07:22:37 +0100

    Updated SwrviceWorker to remove jQuery

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,6 @@
 // we'll version our cache (and learn how to delete caches in
 // some other post)
-const cacheName = 'v1.1::static';
+const cacheName = 'v1.2::static';
 
 self.addEventListener('install', e => {
   // once the SW is installed, go ahead and fetch the resources
@@ -10,20 +10,19 @@ self.addEventListener('install', e => {
       return cache.addAll([
         '/',
         'assets/css/global.min.css',
-        'assets/js/libs/jquery-1.11.3.min.js',
         'assets/js/global.min.js',
         'about/',
         'work/',
         'contact/'
-      ]).then(() => self.skipWaiting());
+      ]).then(() => self.skipWaiting())
     })
-  );
-});
+  )
+})
 
 // when the browser fetches a url, either response with
 // the cached object or go ahead and fetch the actual url
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(res => res || fetch(event.request))
-  );
-});
+  )
+})
