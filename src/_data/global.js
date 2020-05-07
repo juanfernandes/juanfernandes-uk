30adee1d513a90e53489074bdeb7ac49ef1d2c92
Juan Fernandes <juan@fernandes.fyi>
2020-05-07 16:25:25 +0100

    Add randomizer script for ServiceWorker
    - renamed sw.js to service-worker.njk so we can use a Nunjucks for the
    version value
    - moved the sw script to a partials folder

--- src/_data/global.js
+++ src/_data/global.js
@@ -1,3 +1,11 @@
 module.exports = {
-  copyrightYear: new Date().getFullYear()
+  copyrightYear: new Date().getFullYear(),
+
+  random() {
+    const segment = () => {
+      return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    };
+    return `${segment()}-${segment()}-${segment()}`;
+  },
+  now: Date.now()
 };
