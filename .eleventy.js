1de1d01b01b22d5aafeac46d37d32ba3233f7d30
Juan Fernandes <juan@fernandes.fyi>
2023-10-25 15:55:28 +0100

    Initial commit - removes files no longer needed

--- .eleventy.js
+++ .eleventy.js
@@ -4,13 +4,11 @@ const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
 // Import filters
 const dateFilter = require('./src/_filters/date-filter')
 const w3DateFilter = require('./src/_filters/w3-date-filter')
-const getWebmentionsForUrl = require('./src/_filters/getWebmentionsForUrl')
 
 module.exports = function (eleventyConfig) {
   // Filters
   eleventyConfig.addFilter('dateFilter', dateFilter)
   eleventyConfig.addFilter('w3DateFilter', w3DateFilter)
-  eleventyConfig.addFilter('getWebmentionsForUrl', getWebmentionsForUrl)
 
   eleventyConfig.addFilter('randomItem', (arr) => {
     arr.sort(() => {
@@ -24,18 +22,14 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets/imgs')
   eleventyConfig.addPassthroughCopy('src/assets/js')
   eleventyConfig.addPassthroughCopy('src/robots.txt')
-  eleventyConfig.addPassthroughCopy('src/keybase.txt')
   eleventyConfig.addPassthroughCopy('src/humans.txt')
   eleventyConfig.addPassthroughCopy('src/site.webmanifest')
   eleventyConfig.addPassthroughCopy('src/browserconfig.xml')
-  eleventyConfig.addPassthroughCopy('src/service-worker.js')
   eleventyConfig.addPassthroughCopy('src/192.png')
   eleventyConfig.addPassthroughCopy('src/512.png')
   eleventyConfig.addPassthroughCopy('src/apple-touch-icon.png')
   eleventyConfig.addPassthroughCopy('src/favicon.ico')
   eleventyConfig.addPassthroughCopy('src/icon.svg')
-  eleventyConfig.addPassthroughCopy('.well-known/')
-  eleventyConfig.addPassthroughCopy('src/Juan_Fernandes-CV-2022.pdf')
   eleventyConfig.addPassthroughCopy('src/admin/index.html')
 
   eleventyConfig.addCollection('posts', function (collection) {
